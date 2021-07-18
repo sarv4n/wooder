9267498fa5d7550e75fe2c4ec34a71239a99c370
sarv4n <84637530+sarv4n@users.noreply.github.com>
2021-07-18 17:33:25 +0300

    1 commit
    1 commit

---
+++ wooder/js/script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// document.onwheel = function(event){
+//     if (currentSliderNumber != slidesArray.length - 1 && event.deltaY > 0){
+//         nextSlide()
+//     }
+//     else if (currentSliderNumber != 0 && event.deltaY < 0){
+//         previousSlide()
+//     }
+// }
